357f9e420c3c82f80fb5ffe00349e6e6f6e962ad
Songparadise <97575209+Songparadise@users.noreply.github.com>
2022-01-12 16:27:32 +0530

    Update sew.js

--- sew.js
+++ sew.js
@@ -1,10 +1,7 @@
-/*Sew Queen Whatsapp Bot    
-
-Telegram: https://t.me/RavinduManoj
-Facebook: https://www.facebook.com/ravindu.manoj.79
+/*Garfielld Whatsapp Bot    
 Licensed under the  GPL-3.0 License
 
-Coded By Ravindu Manoj
+Coded By Sarith Vimukthi
 */
 let DataPack = require('sew-queen-pro/export/output');
 let SewQueen = require('sew-queen-pro/sources/dc/handler');
